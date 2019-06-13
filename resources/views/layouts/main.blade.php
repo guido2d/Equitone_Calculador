51c84acd612bc272aa4996ac7852ffede2666c8b
Guido Di Salvo <guido.disalvoo@gmail.com>
2019-06-12 23:17:09 -0300

    actualizacion css forzada

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
     <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/cedral.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css?v=1.1') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css?v=1.2') }}">
     <link rel="stylesheet" href="{{ asset('css/responsive.css?v=1.0') }}">
 
     @yield('css')
